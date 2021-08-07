695360f670cca6685d84eec945bd82c1d12d0a1b
putin1108 <88575085+putin1108@users.noreply.github.com>
2021-08-07 14:50:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Em biết mà 🥰. Yêu chị nhiều lắm 😘😘',
     messDesc: 'Nếu thật sự chị cũng muốn em và nghiêm túc với em, xin hãy chấp nhận con người thật của em là ai.',
     btnAccept: 'xin hãy tin em và chấp nhận em, như chị có nói vậy Hope <3',
-    messLink: 'https://www.facebook.com/T.H.T.Long.1108/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/T.H.T.Long.1108/'
 }
