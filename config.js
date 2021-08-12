5191d7ab833cd6ed0868a9c75d38dfbaf8fcc037
putin1108 <88575085+putin1108@users.noreply.github.com>
2021-08-12 14:42:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
-const  CONFIG = {
+const CONFIG = {
     introTitle: 'He luu Minttt',
     introDesc: `Nếu bà không trả lời mà thoát ra thì tui tự mặc định là bà muốn làm người yêu tui luôn nha :>>`,
-    btnIntro: 'Rồi thì tiếp tục đi',
+    btnIntro: 'Rồi hỏi đi nè',
     title: 'Tui có điều này muốn hỏi, bà nhớ phải trả lời thật lòng nhaaa.',
-    desc: 'bà ăn cái gì mà xinh dữ thần luôn dạ trời đấtttt :3 ',
-    btnYes: 'để tui nói bí mật cho nghe hen <33',
+    desc: 'Bà ăn cái gì mà xinh dữ thần luôn dạ trời đấtttt :3 ',
+    btnYes: 'Để tui nói bí mật cho nghe hen <33',
     btnNo: 'Hông biết nữaa :3',
-    question:'trả lời hoặc tui mua trà sữa rồi dìm bà vô đống đồ ăn với trà sữa đó á =.=',
+    question:'Trả lời hoặc tui mua trà sữa rồi dìm bà vô đống đồ ăn với trà sữa đó á =.=',
     btnReply: 'Send <3',
-    reply: 'tui ăn cơm mẹ tui náu thôi, ông có muốn ăn chung hôngg ?',
-    mess: 'tất nhiên là có rồiiii 😘😘',
-    messDesc: ' vậy thôi được, qua dịch tui qua đón bà đi ăn hennnn',
-    btnAccept: 'ok ông luônnnn <3',
+    reply: 'Tui ăn cơm mẹ tui náu thôi, ông có muốn ăn chung hôngg ?',
+    mess: 'Tất nhiên là có rồiiii 😘😘',
+    messDesc: 'Vậy thôi được, qua dịch tui qua đón bà đi ăn hennnn',
+    btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/T.H.T.Long.1108/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
